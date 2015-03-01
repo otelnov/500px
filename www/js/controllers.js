42a1d7dc3650f91b4dfecaa50c5d504db8341dd6
otelnov <otelnov@gmail.com>
2015-03-01 19:22:16 +0200

    like method

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,79 +2,61 @@ angular.module('500px.controllers', [])
 
   .controller('DashCtrl', function ($scope, TDCardDelegate, $ionicLoading, $ionicPlatform, $window) {
 
-    var authUrl = 'https://api.500px.com/api/js-sdk/authorize?sdk_key=b4512a8c50b7f1cbea9765e16d0552095847d76d&redirectUrl=http://localhost:8100/callback.html';
+    //var authUrl = 'https://api.500px.com/api/js-sdk/authorize?sdk_key=b4512a8c50b7f1cbea9765e16d0552095847d76d&redirectUrl=http://localhost/callback';
+    //
+    //$window.document.addEventListener("deviceready", function() {
+    //  var browserRef = window.open("", "_blank");
+    //
+    //  browserRef.addEventListener("loadstart", function(event) {
+    //      if((event.url).indexOf('localhost/callback') === 0) {
+    //          var requestToken = (event.url).split("code=")[1];
+    //          console.log(event.url);
+    //
+    //          browserRef.close();
+    //      }
+    //  }, true);
+    //
+    //  browserRef.addEventListener('exit', function(event) {
+    //      console.error("The sign in flow was canceled");
+    //  }, true);
+    //
+    //  browserRef.addEventListener('message', function(e) {
+    //    console.log(e);
+    //  }, true);
+    //
+    //  browserRef.location.assign(authUrl);
+    //});
 
-    $window.document.addEventListener("deviceready", function() {
-      var browserRef = window.open("", "_blank");
+    //$ionicLoading.show({
+    //  template: 'Loading...'
+    //});
 
-      browserRef.addEventListener("loadstart", function(event) {
-          if((event.url).indexOf('localhost/callback') === 0) {
-              var requestToken = (event.url).split("code=")[1];
-              console.log(event.url);
+    var page = 0;
+    $scope.cards = [];
+    loadMore();
 
-              browserRef.close();
-          }
-      }, true);
+    $scope.like = function (img) {
+      _500px.api('/photos/' + img.id + '/vote', 'post', {vote: 1}, function (response) {
+        console.log(response);
+      });
+    };
 
-      browserRef.addEventListener('exit', function(event) {
-          console.error("The sign in flow was canceled");
-      }, true);
+    $scope.cardDestroyed = function (index) {
+      $scope.cards.splice(index, 1);
+      if (index === 1) {
+        console.log('loadnew');
+        loadMore();
+      }
+    };
 
-      browserRef.addEventListener('message', function(e) {
-        console.log(e);
-      }, true);
+    function loadMore() {
+      $ionicLoading.show({
+        template: 'Loading...'
+      });
+      page++;
 
-      browserRef.location.assign(authUrl);
-    });
-
-    $ionicLoading.show({
-      template: 'Loading...'
-    });
-
-    var page = 1;
-
-    _500px.api('/photos', {feature: 'popular', page: 1, image_size: 3}, function (response) {
-      //console.log('loaded',response.data);
-      $ionicLoading.hide();
-      //cardTypes = response.data.photos;
-
-      $scope.cards = response.data.photos;
-      $scope.current = $scope.cards[$scope.cards.length - 1];
-      //$scope.$apply();
-      //$scope.addCard = function () {
-      //  var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
-      //  newCard.id = Math.random();
-      //  $scope.cards.unshift(angular.extend({}, newCard));
-      //};
-      //
-      //for (var i = 0; i < 3; i++) {
-      //  $scope.addCard();
-      //}
-      //
-      $scope.cardDestroyed = function (index) {
-        //todo: block screen and wait other if it is last item
-        $scope.cards.splice(index, 1);
-        $scope.current = $scope.cards[index - 1];
-        if(index===3){
-          page++;
-          loadMore(page);
-        }
-      };
-
-
-      //$scope.cardSwipedLeft = function (index) {
-      //$scope.addCard();
-
-      //};
-      //$scope.cardSwipedRight = function (index) {
-      //  $scope.addCard();
-      //};
-
-
-    });
-
-    function loadMore(page){
       _500px.api('/photos', {feature: 'popular', page: page, image_size: 3}, function (response) {
+        $ionicLoading.hide();
         $scope.cards = response.data.photos.concat($scope.cards);
       });
     }
