7d575e85f8a5e27c7a7b2a07639f36e8725b92bd
otelnov <otelnov@gmail.com>
2015-03-01 20:02:48 +0200

    auth

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,35 +2,6 @@ angular.module('500px.controllers', [])
 
   .controller('DashCtrl', function ($scope, TDCardDelegate, $ionicLoading, $ionicPlatform, $window) {
 
-    //var authUrl = 'https://api.500px.com/api/js-sdk/authorize?sdk_key=b4512a8c50b7f1cbea9765e16d0552095847d76d&redirectUrl=http://localhost/callback';
-    //
-    //$window.document.addEventListener("deviceready", function() {
-    //  var browserRef = window.open("", "_blank");
-    //
-    //  browserRef.addEventListener("loadstart", function(event) {
-    //      if((event.url).indexOf('localhost/callback') === 0) {
-    //          var requestToken = (event.url).split("code=")[1];
-    //          console.log(event.url);
-    //
-    //          browserRef.close();
-    //      }
-    //  }, true);
-    //
-    //  browserRef.addEventListener('exit', function(event) {
-    //      console.error("The sign in flow was canceled");
-    //  }, true);
-    //
-    //  browserRef.addEventListener('message', function(e) {
-    //    console.log(e);
-    //  }, true);
-    //
-    //  browserRef.location.assign(authUrl);
-    //});
-
-    //$ionicLoading.show({
-    //  template: 'Loading...'
-    //});
-
     var page = 0;
     $scope.cards = [];
     loadMore();
@@ -61,5 +32,33 @@ angular.module('500px.controllers', [])
       });
     }
 
+
+    $scope.login = function () {
+      $ionicPlatform.ready(function () {
+        var browserRef = $window.open('https://api.500px.com/api/js-sdk/authorize?sdk_key=b4512a8c50b7f1cbea9765e16d0552095847d76d', '_blank');
+        browserRef.addEventListener('loadstart', function (event) {
+          if ((event.url).indexOf("http://example.com") === 0) {
+            console.log(event.url);
+
+            //var hash_string = window.location.hash.substring(1),
+            var hash_string = event.url,
+              parts = hash_string.split(','),
+              part,
+              result = {};
+            while (part = parts.shift()) {
+              part = part.split(':');
+              result[part[0]] = part[1] == undefined ? true : part[1];
+            }
+            console.log(result);
+
+            browserRef.close();
+          }
+        });
+        browserRef.addEventListener('exit', function (event) {
+          //deferred.reject("The sign in flow was canceled");
+        });
+
+      });
+    }
 
   });
